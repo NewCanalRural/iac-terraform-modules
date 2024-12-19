40f817287065732202d2b4c304a43c4ac59e970e
edsonoliveira94 <edson13.oliveira@gmail.com>
2024-12-18 23:00:16 -0300

    Update secret version parameter

--- modules/secret_manager/main.tf
+++ modules/secret_manager/main.tf
@@ -36,5 +36,5 @@ resource "google_secret_manager_secret" "secret" {
 
 resource "google_secret_manager_secret_version" "version" {
   secret      = google_secret_manager_secret.secret.id
-  secret_data = var.secret_data
+  payload     = var.secret_data
 }
