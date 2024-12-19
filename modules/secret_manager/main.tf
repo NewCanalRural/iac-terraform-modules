58d0be6f4d635e46a151a5b7f2187166a1b7a1ae
edsonoliveira94 <edson13.oliveira@gmail.com>
2024-12-18 22:06:27 -0300

    Add secret_manager module

---
+++ modules/secret_manager/main.tf
@@ -0,0 +1,40 @@
+resource "google_secret_manager_secret" "secret" {
+  project   = var.project_id
+  secret_id = var.name
+  labels    = var.labels
+  replication {
+    dynamic "auto" {
+      for_each = length(var.user_managed_replication) > 0 ? [] : [1]
+      content {
+        dynamic "customer_managed_encryption" {
+          for_each = var.automatic_replication.kms_key_name != null ? [var.automatic_replication.kms_key_name] : []
+          content {
+            kms_key_name = customer_managed_encryption.value
+          }
+        }
+      }
+    }
+    dynamic "user_managed" {
+      for_each = length(var.user_managed_replication) > 0 ? [1] : []
+      content {
+        dynamic "replicas" {
+          for_each = var.user_managed_replication
+          content {
+            location = replicas.value.location
+            dynamic "customer_managed_encryption" {
+              for_each = replicas.value.kms_key_name != null ? [replicas.value.kms_key_name] : []
+              content {
+                kms_key_name = customer_managed_encryption.value
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "google_secret_manager_secret_version" "version" {
+  secret      = google_secret_manager_secret.secret.id
+  secret_data = var.secret_data
+}
